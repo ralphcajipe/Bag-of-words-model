c577b4906f61b8f615f722b50f575ad15fdae5eb
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-06-02 19:33:41 +0800

    Update BoW_NLP.py

--- BoW_NLP.py
+++ BoW_NLP.py
@@ -30,11 +30,31 @@ print(vocab)
 print("\nFrequency of words:")
 print(bag)
 
-# OUTPUT
-# Vocabulary and their index number:
-# {'this': 1, 'is': 2, 'a': 3, 'test': 4, 'to': 5, 'see': 6, 'if': 7, 'will': 8, 'work.': 9, 'one': 10}
+'''
+OUTPUT:
+Vocabulary and their index number:
+{'this': 1, 'is': 2, 'a': 3, 'test': 4, 'to': 5, 'see': 6, 'if': 7, 'will': 8, 'work.': 9, 'one': 10}
 
-# Frequency of words:
-# {1: 3, 2: 2, 3: 1, 4: 3, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1}
+Frequency of words:
+{1: 3, 2: 2, 3: 1, 4: 3, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1}
 
-# This isn't really the way we would do this in practice, but I hope it gives you an idea of how bag of words works. Notice that we've lost the order in which words appear.
+This isn't really the way we would do this in practice, but I hope it gives you an idea of how bag of words works.
+Notice that we've lost the order in which words appear. 
+*********************************************************************************************************************
+In fact, let's look at how this encoding works for our movie reviews:
+
+positive_review = "I thought the movie was going to be bad but it was actually amazing"
+negative_review = "I thought the movie was going to be amazing but it was actually bad"
+
+pos_bag = bag_of_words(positive_review)
+neg_bag = bag_of_words(negative_review)
+
+print("Positive:", pos_bag)
+print("Negative:", neg_bag)
+
+OUTPUT:
+Positive: {11: 1, 12: 1, 13: 1, 14: 1, 15: 2, 16: 1, 5: 1, 17: 1, 18: 1, 19: 1, 20: 1, 21: 1, 22: 1}
+Negative: {11: 1, 12: 1, 13: 1, 14: 1, 15: 2, 16: 1, 5: 1, 17: 1, 22: 1, 19: 1, 20: 1, 21: 1, 18: 1}
+
+We can see that even though these sentences have a very different meaning they are encoded exaclty the same way. 
+Obviously, this isn't going to work well. We have to look at some other methods like "Integer Encoding".
