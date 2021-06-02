7bd885ae3dbdae65d0a663b2f13caf539c17c7cb
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-06-02 18:01:30 +0800

    Add files via upload

---
+++ BoW_NLP.py
@@ -0,0 +1,29 @@
+# Bag-of-words model in NLP
+
+vocab = {}  # maps word to integer representing it
+word_encoding = 1
+def bag_of_words(text):
+  global word_encoding
+
+  words = text.lower().split(" ")  # create a list of all of the words in the text, we'll assume there is no grammar in our text for this example
+  bag = {}  # stores all of the encodings and their frequency
+
+  for word in words:
+    if word in vocab:
+      encoding = vocab[word]  # get encoding from vocab
+    else:
+      vocab[word] = word_encoding
+      encoding = word_encoding
+      word_encoding += 1
+    
+    if encoding in bag:
+      bag[encoding] += 1
+    else:
+      bag[encoding] = 1
+  
+  return bag
+
+text = "this is a test to see if this test will work is is test a a"
+bag = bag_of_words(text)
+print(bag)
+print(vocab)
